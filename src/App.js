2757c3440586041113ded3e2557d5f19400a0b22
IgorGryp <igor.gryp@gmail.com>
2023-04-17 19:25:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  function getRandomQuote(quotes) {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+  }
+
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((json) => {
+        setQuotes(json);
+        setQuote(json[0]);
+      });
+  }, []);
+
+  function getNewQuote() {
+    setQuote(getRandomQuote(quotes));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="quote-section">
+        {/* <h1>Quote Generator</h1> */}
+
+        <div className="quote-div">
+          <div className="quote">
+            <h3>“{quote?.text}“</h3>
+            <p className="author">- {quote?.author} -</p>
+          </div>
+          <button onClick={getNewQuote}>Next Quote</button>
+        </div>
+      </section>
     </div>
   );
 }
